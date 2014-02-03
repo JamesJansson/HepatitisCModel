46651c9afb183c5c21550225ef26daf2f62d0f0d
JamesJansson <jamesjansson@gmail.com>
2014-02-03 14:52:20 +1100

    Description of groups to try

---
+++ FirstCode.m
@@ -0,0 +1,20 @@
+% Sparse random network
+% Small world network
+% Sparse network with dense subgraph <- most realistic for cold, flu etc
+
+%http://arxiv.org/ftp/arxiv/papers/1302/1302.4024.pdf
+
+TotalNodes=1000000;
+ConnectionsPerNode=2*3*2;
+
+
+
+
+
+% Groups to try
+
+% Each person has 2 groups with 6 connections, leading to 7 people per group
+% Each person has 3 groups with 4 connections, leading to 5 people per group
+% Each person has 4 groups with 3 connections, leading to 4 people per group
+% Each person has 6 groups with 2 connections, leading to 3 people per group
+% Each person has 12 groups with 2 connections, leading to 2 people per group (Each person has 12 connections) 
