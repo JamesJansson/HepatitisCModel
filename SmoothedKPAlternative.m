a452bc35ff96895bd62a590b4af9e6949d740da3
JamesJansson <jamesjansson@gmail.com>
2014-05-01 00:31:39 +1000

    a

--- SmoothedKPAlternative.m
+++ SmoothedKPAlternative.m
@@ -1,6 +1,18 @@
 function [NonSurvivalProbability TimeBounds]=SmoothedKPAlternative (SurvivalTimeData, Censored)
 %note time bounds start with 0 and end wtih -1 (to indicate infinity)
 
-%Determine the time between surival points
+% Order the data
 [SurvivalTimeData, I]=sort(SurvivalTimeData);
 Censored=Censored(I);
+NotCensored=not(Censored);
+
+% Determine the population time between surival points
+Pos=0;
+for i=NotCensored
+    Pos=Pos+1;
+    if i==1
+        % Determine the probabilty of death per 
+        
+    end
+end
+
